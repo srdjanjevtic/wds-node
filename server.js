f9633ba54a35a67674815f9a758d564594b2faef
srdjanjevtic <srdjanjevticle@gmail.com>
2019-10-13 18:21:27 +0200

    books create/index/new

--- server.js
+++ server.js
@@ -4,14 +4,19 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 const db = mongoose.connection
@@ -19,6 +24,8 @@ db.on('error', error => console.log(error))
 db.once('open', () => console.log('connected to database'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`server starts on port ${port}`))
